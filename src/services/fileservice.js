62ab2b981dd9c7bc1d7cc5cd7829f2d06970354d
davidcatalan9 <dcatalan@gmail.com>
2024-11-07 20:16:30 -0300

    Creando proyecto d crud

--- src/services/fileservice.js
+++ src/services/fileservice.js
@@ -2,3 +2,11 @@ import fs from "fs";
 import path from "path";
 
 const dataFilePath = path.join(__dirname, "../data/data.json");
+
+const createFile = () => { 
+    try {
+        fs.writeFile(dataFilePath, JSON.stringify(data))
+    } catch (error) {
+        
+    }
+}
